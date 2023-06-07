ed17d651cb81bd9feef9c19559ce6a396a6f54e3
Mohamed Said <Mohamedsaid.2001@hotmail.com>
2023-06-07 02:31:21 +0200

    first commit

---
+++ private/controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * home controller
+ */
+class Home extends Controller
+{
+    function index()
+    {
+        echo "this is the home controller";
+    }
+}
---
+++ private/controllers/Party.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * party controller
+ */
+class Party extends Controller
+{
+    function index($id = null)
+    {
+        echo 'party';
+    }
+}
